b7f89e9d91fb8d4d2d6021cfcfed4c677846f183
dpersoneni <61434967+dpersoneni@users.noreply.github.com>
2020-11-18 17:46:27 +0100

    Delete WaterGlassAdapter.java

--- prog2/music/WaterGlassAdapter.java
+++
@@ -1,8 +0,0 @@
-package it.unimi.di.prog2.music;
-
-public class WaterGlassAdapter extends WaterGlass implements MusicalInstrument {
-    @Override
-    public String play() {
-        return tap();
-    }
-}
